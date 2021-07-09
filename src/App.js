b6382a761fb862cfb474428d1f8343fcda25bf06
Bruhkamargo <wwwbrunocamargo@hotmail.com>
2021-07-09 02:30:31 -0300

    new

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ function App () {
   return (
     <div className='Body-Div'>
       <div className='div-menu'>     
-        <h1 className='menu-tit'>Menu:</h1> 
-        <button className='Button-menu'>IMC</button>
-        <button className='Button-menu'>Bt_1</button>
+        <div className='Header-menu'>
+          <h1 className='menu-tit'>Menu:</h1>
+        </div>
+        <button className='Button-menu' title='Índice de Massa Corporal'>IMC</button>
+        <button className='Button-menu' title='Uma Repetição Máxima'>1 - RM</button>
         <button className='Button-menu'>Bt_2</button>
+        <div className='Footer-menu'>
+          <a to={'https://github.com/Bruhkamargo'}></a>
+        </div>
       </div>
     </div>
   );
