23340637959d16e3e522aeabc9313056577faf8f
Bruhkamargo <wwwbrunocamargo@hotmail.com>
2021-07-19 00:24:09 -0300

    nova aparencia

--- src/App.js
+++ src/App.js
@@ -9,60 +9,42 @@ function App () {
 
   const bt_1 = () => {
     document.getElementById('ID-menu').style.display = 'none';
-    document.getElementById('ID-imc').style.display = 'flex';
-  }
-  const retornar = () => {
-    document.getElementById('ID-menu').style.display = 'flex';
-    document.getElementById('ID-imc').style.display = 'none';
+    document.getElementById('ID-IMC').style.display = 'flex';
   }
 
   return (
     <div className='Body-Div'>
-      <div id='ID-menu' className='div-menu'>     
-        <div className='Header-menu'>
-          <h1 className='menu-tit'>Menu:</h1>
-        </div>
+      <h1>Menu:</h1>
+      <div id='ID-menu' className='Menu'>        
         <button
           className='Button-menu'
           title='Índice de Massa Corporal'
           onClick={bt_1}>IMC</button>
         <button className='Button-menu' title='Uma Repetição Máxima'>1 - RM</button>
         <button className='Button-menu'>Bt_2</button>
-        <div className='Footer-menu'>
-          <a target="_black" rel="noreferrer noopener" href="https://www.instagram.com/k1bruno/">Instagram </a>
-          <a target="_black" rel="noreferrer noopener" href="https://github.com/Bruhkamargo">GitHub </a>          
-          <a target="_black" rel="noreferrer noopener" href="https://api.whatsapp.com/send?phone=5549998193608">WhatsApp </a>          
-        </div>
-      </div>
-      <div id='ID-imc' className='div-imc'> 
-        <div className='Header-menu'>
-          <h1 className='menu-tit'>IMC:</h1>
-        </div>
-        <div className='div-imc-form'>
-          <label>Sexo:
-            <label> Masc. 
-              <input type='Radio' value='sex_1' checked={Sex?true:false} onChange={() => {Sex?setSex(false):setSex(true)}}/>
-            </label>
-            <label> Fem. 
-              <input type='Radio' value='sex_2' checked={Sex?false:true} onChange={() => {Sex?setSex(false):setSex(true)}}/>
-            </label>
-          </label>
-          <label>Peso:          
-            <input type='number' value={Wei} onChange={()=>{setHei()}}></input>
-          </label>
-          <label>Altura:          
-            <input type='number' value={Hei} onChange={()=>{setWei()}}></input>
-          </label>
-          <div className='div-imc-result'>
-            <h2>Resultados:</h2>
-            <label>IMC:</label>
-            <label>IMC:</label>
-          </div>
-        </div>
-        <div className='Footer-menu'>
-          <button className='Footer-menu-bt' onClick={retornar}>Voltar</button>
-        </div>
-
+      </div>  
+      <div id='ID-IMC' className='IMC'>
+        <span>
+          <label>Sexo</label>
+          <label>Masculino
+            <input type='Radio' value='sex_1' checked={Sex?true:false} onChange={() => {Sex?setSex(false):setSex(true)}}/></label>
+          <label>Feminino
+            <input type='Radio' value='sex_1' checked={Sex?false:true} onChange={() => {Sex?setSex(false):setSex(true)}}/></label>
+        </span>
+        <span>
+          <label>Peso: </label>
+          <input type='Number' value={Wei} onChange={(e)=>setWei(e.target.value)}></input>
+        </span>
+        <span>
+          <label>Altura: </label>
+          <input type='Number' value={Hei} onChange={(e)=>setHei(e.target.value)}></input>
+        </span>       
+        <button>Calcular</button>
+      </div>  
+      <div className='Footer'>
+        <a target="_black" rel="noreferrer noopener" href="https://www.instagram.com/k1bruno/">Instagram </a>
+        <a target="_black" rel="noreferrer noopener" href="https://github.com/Bruhkamargo">GitHub </a>          
+        <a target="_black" rel="noreferrer noopener" href="https://api.whatsapp.com/send?phone=5549998193608">WhatsApp </a>          
       </div>
     </div>
   );
