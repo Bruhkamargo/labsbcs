b651073a18add548c461c94432800f2d6af4e337
Bruhkamargo <wwwbrunocamargo@hotmail.com>
2021-07-20 15:59:14 -0300

    ...

--- src/App.js
+++ src/App.js
@@ -8,13 +8,23 @@ function App () {
   const [Wei, setWei] = useState(0)
 
   const bt_1 = () => {
+    document.getElementById('ID-Menu-tit').innerText = 'IMC:';
     document.getElementById('ID-menu').style.display = 'none';
+    document.getElementById('ID-bt-return').style.display = 'block';
     document.getElementById('ID-IMC').style.display = 'flex';
+  }
+
+  const bt_return = () => {
+    document.getElementById('ID-Menu-tit').innerText = 'Menu:';
+    document.getElementById('ID-menu').style.display = 'flex';
+    document.getElementById('ID-bt-return').style.display = 'none';
+    document.getElementById('ID-IMC').style.display = 'none';
   }
 
   return (
     <div className='Body-Div'>
       <h1 id='ID-Menu-tit' className='Menu-tit'>Menu:</h1>
+      {/* Menu */}
       <div id='ID-menu' className='Menu'>        
         <button
           className='Button-menu'
@@ -23,24 +33,30 @@ function App () {
         <button className='Button-menu' title='Uma Repetição Máxima'>1 - RM</button>
         <button className='Button-menu'>Bt_2</button>
       </div>  
+
+      {/* IMC */}
+
       <div id='ID-IMC' className='IMC'>
-        <span>
+        <div className='imc-sex'>
           <label>Sexo</label>
           <label>Masculino
             <input type='Radio' value='sex_1' checked={Sex?true:false} onChange={() => {Sex?setSex(false):setSex(true)}}/></label>
           <label>Feminino
             <input type='Radio' value='sex_1' checked={Sex?false:true} onChange={() => {Sex?setSex(false):setSex(true)}}/></label>
-        </span>
-        <span>
+        </div>
+        <div className='imc-input'>
           <label>Peso: </label>
           <input type='Number' value={Wei} onChange={(e)=>setWei(e.target.value)}></input>
-        </span>
-        <span>
+        </div>
+        <div className='imc-input'>
           <label>Altura: </label>
           <input type='Number' value={Hei} onChange={(e)=>setHei(e.target.value)}></input>
-        </span>       
-        <button>Calcular</button>
+        </div>       
+        <button className='imc-input-bt'>Calcular</button>
       </div>  
+      
+      <button id='ID-bt-return' className='bt-return' onClick={bt_return}>⮪</button>
+
       <div className='Footer'>
         <a target="_black" rel="noreferrer noopener" href="https://www.instagram.com/k1bruno/">Instagram </a>
         <a target="_black" rel="noreferrer noopener" href="https://github.com/Bruhkamargo">GitHub </a>          
