f21c9c80f628d6c5d7d214ce4d31b9321536b245
Bruhkamargo <wwwbrunocamargo@hotmail.com>
2021-09-04 22:37:07 -0300

    Back IMC

--- src/IMC/IMC.js
+++ src/IMC/IMC.js
@@ -7,9 +7,49 @@ function IMC(params) {
   const [Hei, setHei] = useState()
   const [Wei, setWei] = useState()
 
+  const check_imc_calc = () => {
+    if (Hei === 0 || Hei === undefined || Wei === 0 || Wei === undefined) {
+      alert('Preencha todos os Dados')
+    } else {
+      imc_calc()
+    }
+  }
+
   const imc_calc = () => {
+    document.getElementById('ID-Menu-tit').innerText = 'Resultado';
     document.getElementById('ID-IMC').style.display = 'none'
     document.getElementById('ID-IMC_R').style.display = 'Flex'
+
+    let imc = Wei / ((Hei / 100) **2)
+
+    if (Sex === true) {
+      if (imc < 20.69) {
+        document.getElementById('ID-sla3').innerText = 'IMC Abaixo do Normal'        
+      } else if (imc <= 26.40) {
+        document.getElementById('ID-sla3').innerText = 'IMC Normal'        
+      } else if (imc <= 27.80) {
+        document.getElementById('ID-sla3').innerText = 'IMC Acima do Peso'        
+      } else if (imc < 32.30) {
+        document.getElementById('ID-sla3').innerText = 'IMC Obesidade'        
+      } else {        
+        document.getElementById('ID-sla3').innerText = 'IMC Obesidade Morbida'
+      }    
+    } else if (Sex === false) {
+      if (imc < 19.09) {
+        document.getElementById('ID-sla3').innerText = 'IMC Abaixo do Normal'        
+      } else if (imc <= 25.80) {
+        document.getElementById('ID-sla3').innerText = 'IMC Normal'        
+      } else if (imc <= 27.30) {
+        document.getElementById('ID-sla3').innerText = 'IMC Acima do Peso'        
+      } else if (imc < 32.30) {
+        document.getElementById('ID-sla3').innerText = 'IMC Obesidade'        
+      } else {        
+        document.getElementById('ID-sla3').innerText = 'IMC Obesidade Morbida'
+      } 
+    }
+
+    document.getElementById('ID-sla').innerText = `${imc.toFixed(2)}`
+
   }
 
   return (
@@ -39,7 +79,7 @@ function IMC(params) {
           title={'Altura em cm'}
           onChange={(e) => setHei(e.target.value)}></input>
       </div>
-      <button className='imc-input-bt' onClick={imc_calc}>Calcular</button>
+      <button className='imc-input-bt' onClick={check_imc_calc}>Calcular</button>
 
     </div>
   )
--- src/IMC/IMC_RESULT.js
+++ src/IMC/IMC_RESULT.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './IMC_RESULT.css';
 
 function IMC_RESULT(params) {
-    return (
-        <div id='ID-IMC_R' className='IMC_R'>
-            <span>Hello Word</span>
-        </div>
-    )
+  return (
+    <div id='ID-IMC_R' className='IMC_R'>
+      <div className='IMC-div-top'>
+        <label>Pontos de IMC: <span id='ID-sla'></span></label>
+        <label>Classificação do IMC: <span id='ID-sla3'></span></label>
+      </div>
+    </div>
+  )
 }
 
 export default IMC_RESULT
